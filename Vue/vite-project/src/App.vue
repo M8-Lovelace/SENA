f92c5c313615558dd94b32c465a7f3695f31f388
J6-Lovelace <lauritasusano.10@gmail.com>
2023-02-02 11:56:06 -0500

    02-02-23

---
+++ Vue/vite-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ num }}</h1>
+</template>
+
+<script>
+import { ref } from 'vue';
+  export default {
+    setup() {
+      let num = ref(18);
+      return {
+        num,
+      };
+    }
+}
+</script>
+
+<style>
+
+</style>
