ac31b2abbb10c3c42ee64e01bb53808c4a18477d
J6-Lovelace <lauritasusano.10@gmail.com>
2023-03-07 14:41:54 -0500

    07-03-23

--- Node/Example/app.js
+++ Node/Example/app.js
@@ -1,72 +1,81 @@
-import express from "express";
+// Importamos las librerías necesarias
+import * as dotenv from 'dotenv';
+import express from 'express';
+import { Configuration, OpenAIApi } from 'openai';
+import {v4 as uuidv4} from 'uuid';
 
+const PORT = process.env.PORT || 8080
+
+// Cargamos las variables de entorno
+dotenv.config();
+
+// Configuramos el servidor
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
 
-const PORT = process.env.PORT || 8080
+let myuuid = uuidv4();
+let consultas = {}
 
-const users = [
-  {
-    "_id": "1",
-    "email": "mariana@prueba.com",
-    "password": "$00000",
-    "document": "1111111155",
-    "name": "MARIANAS",
-    "rol": "ADMIN",
-    "ficha": "343433",
-    "phone": "34343434",
-    "state": "1",
-    "createdAt": "2022-06-23T01:45:51.854Z",
-    "__v": 0,
-    "photo": "http://res.cloudinary.com/dyocmaqis/image/upload/v1656466235/nwc8wd0t7pvxcfi8grpr.png"
-},
-{
-    "_id": "2",
-    "email": "nany@prueba.com",
-    "password": "1234567",
-    "document": "11111111",
-    "name": "NANY",
-    "rol": "APRENDIZ",
-    "ficha": "343433",
-    "phone": "34343434",
-    "state": "1",
-    "__v": 0
+// Configuramos la API de OpenAI
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Creamos la instancia de la API
+const openai = new OpenAIApi(configuration);
+
+// Función para generar texto con OpenAI API
+async function runCompletion(texto) {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: texto}],
+  });
+  return completion.data.choices[0].message.content;
 }
-]
 
-app.get("/user", (req, res) => {
-  res.send(users);
-})
+// Agregar una nueva consulta
+app.post('/consultas/:usuario',async (req, res) => {
+  const { usuario } = req.params;
+  const { texto } = req.body;
+  const rta= await runCompletion(texto);
+  const uuid = uuidv4();
 
-app.get("/user/:id", (req, res) => {
-  const user = users.find((user) => user._id === req.params.id);
-  if(!user) res.status(404).send("The user with the given ID was not found");
-  res.send(user);
-})
+  consultas[uuid] = {  usuario, texto, rta};
 
-app.post("/user", (req, res) => {
-  const user = req.body;
-  user._id = `${users.length + 1}`;
-  users.push(user);
-  res.send(`User with the name ${user.name} added to the database`);
-})
+  res.send({ consultas });
+});
 
-app.put("/user/:id", (req, res) => {
-  const user = users.find((user) => user._id === req.params.id);
-  if(!user) res.status(404).send("The user with the given ID was not found");
-  user.name = req.body.name;
-  res.send(`User with the name ${user.name} has been updated`);
-})
+// Listar todas las consultas
+app.get('/consultas', (req, res) => {
+  const listaConsultas = Object.values(consultas);
+  res.send(listaConsultas);
+});
 
-app.delete("/user/:id", (req, res) => {
-  const user = users.find((user) => user._id === req.params.id);
-  if(!user) res.status(404).send("The user with the given ID was not found");
-  user.state = "0";
-  res.send(`User with the name ${user.name} has been deleted`);
-})
+app.get('/consultas/contiene', (req, res) => {
+  const palabra = req.query.palabra;
+  const consultasContiene = Object.values(consultas)
+    .filter(c => c.texto.includes(palabra));
+  res.send(consultasContiene);
+});
 
+// Listar consultas de un usuario
+app.get('/consultas/usuario/', (req, res) => {
+  const usuario = req.query.usuario;
+  const consultasUsuario = Object.values(consultas)
+    .filter(c => c.usuario === usuario);
+  res.send(consultasUsuario);
+});
+
+// Eliminar una consulta
+app.delete('/consultas/:uuid', (req, res) => {
+  const { uuid } = req.params;
+  delete consultas[uuid];
+  res.send({ consultas });
+});
+
+// Mensaje de bienvenida al servidor, indica que está activo
 app.listen(PORT, () => {
-  console.log("Example app listening on port 3000!");
+  console.log(`Example app listening on port ${process.env.PORT}!`);
 })
