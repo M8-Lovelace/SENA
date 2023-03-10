8e12dc3846be60305bb6d99fb18ae776125a7b24
Laura Susano <lauritasusano.10@gmail.com>
2023-03-10 17:42:46 -0500

    10-03-23

--- Vue/quasar/src/App.vue
+++ Vue/quasar/src/App.vue
@@ -1,20 +1,6 @@
-<script setup>
-
-</script>
-
 <template>
-  <div class="q-pa-md q-gutter-sm ">
-    <q-btn color="white" text-color="black" label="Standard" />
-    <q-btn color="primary" label="Primary" />
-    <q-btn color="secondary" label="Secondary" />
-    <q-btn color="amber" glossy label="Amber" />
-    <q-btn color="brown-5" label="Brown 5" />
-    <q-btn color="deep-orange" glossy label="Deep Orange" />
-    <q-btn color="purple" label="Purple" />
-    <q-btn color="black" label="Black" />
-  </div>
+    <Layout></Layout>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
+<script setup>
+import Layout from "./components/Layout.vue";
+</script>
---
+++ Vue/quasar/src/components/Sidebar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <q-list class="iconsList">
+            <q-item clickable v-ripple class="item">
+                <q-item-section avatar>
+                    <router-link to="/" class="row">
+                        <img src="../assets/home.png" alt="" width="30" height="30">
+                    </router-link>
+                </q-item-section>
+            </q-item>
+            <q-item clickable v-ripple class="item">
+                <q-item-section>
+                    <router-link to="/login" class="row">
+                        <img src="../assets/login.png" alt="" width="30" height="30">
+                    </router-link>
+                </q-item-section>
+            </q-item>
+            <q-item clickable v-ripple class="item">
+                <q-item-section>
+                    <router-link to="/about" class="row link">
+                        <img src="../assets/icon.png" alt="" width="30" height="30">
+                    </router-link>
+                </q-item-section>
+            </q-item>
+        </q-list>
+    </div>
+</template>
+
+<style scoped lang="sass">
+.iconsList
+    margin-top: 15px
+
+.item
+    padding-left: 25px
+    padding-top: 15px
+    padding-bottom: 15px
+</style>
